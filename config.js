900650902cafa792100253e62091d1039888ff8b
nxthangg <119399948+nxthangg@users.noreply.github.com>
2023-06-20 16:02:35 +0700

    HPBD

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Chesee';
+const giftUrl = 'http://facebook.com/nxthangg';
+const eventName = 'HAPPY BIRTHDAY';
+const titleCard = 'Chúc mừng sinh nhật Em';
+const contentCard = 'Chúc Ebee của anh sinh nhật vui vẻ luôn tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Anh yêu em ♥';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'ebe.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
